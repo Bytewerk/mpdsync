e85dbb64944711f7f979236ed1d7a68e7773482b
sqozz <sqozz@geekify.de>
2015-04-26 04:09:45 +0200

    Added an instant sync if the switch gets toggled

--- mpdsync.py
+++ mpdsync.py
@@ -1,4 +1,5 @@
 from mpd import MPDClient
+from time import sleep
 import RPi.GPIO as GPIO
 
 currentSong = None
@@ -9,6 +10,24 @@ def setupGPIO():
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(7, GPIO.IN)
 
+def fadeOver(newSong):
+	oldVol = int(selfMPD.status()["volume"]
+	selfMPD.addid(newSong, 1)
+	#fade out
+	for vol in range(oldVol, 1, -1):
+		selfMPD.setvol(vol)
+		sleep(0.1)
+
+	selfMPD.playid(1)
+	bingoMPDPlayTime = bingoMPD.status()["time"].split(":")[0]
+	selfMPD.seekcur(bingoMPDPlayTime)
+
+	#fade in
+	for vol in range(0, oldVol, 1):
+		selfMPD.setvol(vol)
+		sleep(0.1)
+
+
 def connect():
 	global bingoMPD
 	global selfMPD
@@ -18,7 +37,10 @@ def connect():
 	selfMPD.connect("localhost", 6600)
 
 def updateSong():
+	print("don't call me anymore, fool!")
+	return
 	if GPIO.input(7):
+		# Sync is on
 		global currentSong
 		try:
 			try:
@@ -35,6 +57,7 @@ def updateSong():
 			connect()
 			updateSong()
 	else:
+		# Sync is off
 		pass
 
 def close():
@@ -50,8 +73,26 @@ def close():
 setupGPIO()
 connect()
 updateSong()
+currentSong = bingoMPD.currentsong()["file"]
+oldSong = currentSong
+currentSwitchState = GPIO.input(7) # True == Sync
+oldSwitchState = currentSwitchState
 while True:
-	print("waiting for songchange")
-	updateSong()
-	bingoMPD.idle("player")
+	# If the song has changed, add it to position 0 and play it
+	if currentSong != oldSong:
+		try:
+			selfMPD.delete(0)
+		except:
+			pass
+		selfMPD.addid(currentSong, 0)
+		selfMPD.play(0)
+		oldSong = currentSong
+	currentSong = bingoMPD.currentsong()["file"]
+
+	# If the switch gets toggled from "custom play mode" to "sync mode" then fade over to the current playing song
+	if currentSwitchState != oldSwitchState and currentSwitchState:
+		fadeOver(currentSong)
+		oldSwitchState = currentSwitchState
+	currentSwitchState = GPIO.input(7)
+
 close()
