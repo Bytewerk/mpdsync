a2460890bc8adc9ee75c8df0893d7ec3f0bcaffd
Sqozz <sqozz@geekify.de>
2015-04-26 03:24:07 +0000

    Made it work

--- mpdsync.py
+++ mpdsync.py
@@ -11,54 +11,45 @@ def setupGPIO():
 	GPIO.setup(7, GPIO.IN)
 
 def fadeOver(newSong):
-	oldVol = int(selfMPD.status()["volume"]
-	selfMPD.addid(newSong, 1)
+	global selfMPD
+	global bingoMPD
+	oldVol = int(selfMPD.status()["volume"])
 	#fade out
 	for vol in range(oldVol, 1, -1):
 		selfMPD.setvol(vol)
-		sleep(0.1)
+		sleep(0.05)
 
-	selfMPD.playid(1)
+	selfMPD.clear()
+	selfMPD.addid(newSong, 0)
+	selfMPD.play(0)
 	bingoMPDPlayTime = bingoMPD.status()["time"].split(":")[0]
 	selfMPD.seekcur(bingoMPDPlayTime)
 
 	#fade in
 	for vol in range(0, oldVol, 1):
 		selfMPD.setvol(vol)
-		sleep(0.1)
+		sleep(0.05)
 
 
 def connect():
+	print("Connecting...")
 	global bingoMPD
 	global selfMPD
 	bingoMPD = MPDClient()
 	bingoMPD.connect("music.bingo", 6600)
 	selfMPD = MPDClient()
 	selfMPD.connect("localhost", 6600)
+	print("Connected!")
 
-def updateSong():
-	print("don't call me anymore, fool!")
-	return
-	if GPIO.input(7):
-		# Sync is on
-		global currentSong
-		try:
-			try:
-				selfMPD.delete(0)
-			except:
-				pass
-			currentSong = bingoMPD.currentsong()["file"]
-			selfMPD.addid(currentSong, 0)
-			print("updated song")
-			selfMPD.play(0)
-		except:
-			print("got an exception in updateSong()")
-			close()
-			connect()
-			updateSong()
-	else:
-		# Sync is off
+def updateSong(newSong):
+	global bingoMPD
+	global selfMPD
+	try:
+		selfMPD.delete(0)
+	except:
 		pass
+	selfMPD.addid(newSong, 0)
+	selfMPD.play(0)
 
 def close():
 	try:
@@ -70,29 +61,43 @@ def close():
 		print("got an exception in close()")
 		pass
 
+print("Setting up GPIOs")
 setupGPIO()
 connect()
-updateSong()
-currentSong = bingoMPD.currentsong()["file"]
+print("Fetching remote song")
+currentSong = bingoMPD.currentsong().get("file")
 oldSong = currentSong
+print("Clearing old playlist")
+selfMPD.clear()
+print("Playing remote song: " + currentSong)
+if len(currentSong) > 0:
+	updateSong(currentSong)
+print("Sync playtime to remote MPD")
+bingoMPDPlayTime = bingoMPD.status()["time"].split(":")[0]
+selfMPD.seekcur(bingoMPDPlayTime)
+
 currentSwitchState = GPIO.input(7) # True == Sync
 oldSwitchState = currentSwitchState
 while True:
 	# If the song has changed, add it to position 0 and play it
-	if currentSong != oldSong:
-		try:
-			selfMPD.delete(0)
-		except:
-			pass
-		selfMPD.addid(currentSong, 0)
-		selfMPD.play(0)
+	if currentSong != oldSong and currentSwitchState == 1:
+		if len(currentSong) > 0:
+			print("Remote song changed, syncing...")
+			updateSong(currentSong)
+		else:
+			print("Remote song is empty, skipping...")
 		oldSong = currentSong
-	currentSong = bingoMPD.currentsong()["file"]
 
 	# If the switch gets toggled from "custom play mode" to "sync mode" then fade over to the current playing song
-	if currentSwitchState != oldSwitchState and currentSwitchState:
-		fadeOver(currentSong)
-		oldSwitchState = currentSwitchState
-	currentSwitchState = GPIO.input(7)
+	if (currentSwitchState != oldSwitchState):
+		if (currentSwitchState == 1):
+			print("Switch changed to sync mode, going to fade now...")
+			fadeOver(currentSong)
+			oldSwitchState = currentSwitchState
+		else:
+			print("Switch changed to manual mode, good luck!")
+			oldSwitchState = currentSwitchState
 
+	currentSong = bingoMPD.currentsong().get("file")
+	currentSwitchState = GPIO.input(7)
 close()
